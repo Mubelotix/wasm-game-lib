5fdd7e96b141a08724377341a6ac7857eeab5ed7
Mubelotix <mubelotix@gmail.com>
2020-02-26 11:57:11 +0100

    added custom font support

--- src/graphics/font.rs
+++ src/graphics/font.rs
@@ -1,3 +1,5 @@
+use wasm_bindgen::JsValue;
+
 pub struct Font {
     pub name: String
 }
@@ -97,5 +99,56 @@ impl Font {
         Font {
             name: String::from("Impact"),
         }
+    }
+
+    pub async fn load(url: &str) -> Result<Font, JsValue> {
+        use web_sys::FontFace;
+        use web_sys::window;
+        use wasm_bindgen_futures::JsFuture;
+
+        let window = window().unwrap();
+
+        let crypto = window.crypto()?;
+        let mut randoms = [0; 25];
+        crypto.get_random_values_with_u8_array(&mut randoms)?;
+        let mut family_name = String::new();
+        for random in randoms.iter() {
+            family_name.push(match random % 26 {
+                0 => 'a',
+                1 => 'b',
+                2 => 'c',
+                3 => 'd',
+                4 => 'e',
+                5 => 'f',
+                6 => 'g',
+                7 => 'h',
+                8 => 'i',
+                9 => 'j',
+                10 => 'k',
+                11 => 'l',
+                12 => 'm',
+                13 => 'n',
+                14 => 'o',
+                15 => 'p',
+                16 => 'q',
+                17 => 'r',
+                18 => 's',
+                19 => 't',
+                20 => 'u',
+                21 => 'v',
+                22 => 'w',
+                23 => 'x',
+                24 => 'y',
+                _ => 'z'
+            });
+        }
+        let font = FontFace::new_with_str(&family_name, &format!("url({})", url))?;
+        JsFuture::from(font.load()?).await?;
+
+        window.document().unwrap().fonts().add(&font)?;
+
+        Ok(Font {
+            name: family_name
+        })
     }
 }
