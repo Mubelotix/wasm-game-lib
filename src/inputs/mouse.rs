6e1d411b06dac9d30c5a337fa923f4dbbfca4559
Mubelotix <mubelotix@gmail.com>
2020-06-04 11:22:55 +0200

    switch mutex to atomics

--- src/inputs/mouse.rs
+++ src/inputs/mouse.rs
@@ -11,24 +11,25 @@ pub enum MouseEvent {
 }
 
 use lazy_static::lazy_static;
+use std::sync::atomic::{AtomicBool, Ordering::Relaxed};
 use std::sync::Mutex;
 
 lazy_static! {
-    static ref IS_RECORDING_MOUSE_EVENTS: Mutex<bool> = Mutex::new(false);
-    static ref IS_MOUSE_DOWN: Mutex<bool> = Mutex::new(false);
-    static ref IS_MOUSE_IN_CANVAS: Mutex<bool> = Mutex::new(true);
+    static ref IS_RECORDING_MOUSE_EVENTS: AtomicBool = AtomicBool::new(false);
+    static ref IS_MOUSE_DOWN: AtomicBool = AtomicBool::new(false);
+    static ref IS_MOUSE_IN_CANVAS: AtomicBool = AtomicBool::new(true);
     static ref MOUSE_POSITION: Mutex<(u32, u32)> = Mutex::new((0,0));
 }
 
 /// Return true if your program already called [start_recording_mouse_events()](fn.start_recording_mouse_events.html) in the past.
 pub fn are_mouse_events_recorded() -> bool {
-    *IS_RECORDING_MOUSE_EVENTS.lock().unwrap()
+    IS_RECORDING_MOUSE_EVENTS.load(Relaxed)
 }
 
 /// Return false if the mouse is outsite the tab.
 /// Make sure you called [start_recording_mouse_events()](fn.start_recording_mouse_events.html) before.
 pub fn is_mouse_in_canvas() -> bool {
-    *IS_MOUSE_IN_CANVAS.lock().unwrap()
+    IS_MOUSE_IN_CANVAS.load(Relaxed)
 }
 
 /// Start recording mouse events in real time.
@@ -43,7 +44,7 @@ pub fn start_recording_mouse_events() {
         let window = window().unwrap();
 
         let event = Closure::wrap(Box::new(move |_event: web_sys::MouseEvent| {
-            *IS_MOUSE_DOWN.lock().unwrap() = true;           
+            IS_MOUSE_DOWN.store(true, Relaxed);        
         }) as Box<dyn FnMut(web_sys::MouseEvent)>);
         window
             .add_event_listener_with_callback("mousedown", event.as_ref().unchecked_ref())
@@ -51,7 +52,7 @@ pub fn start_recording_mouse_events() {
         event.forget();
 
         let event = Closure::wrap(Box::new(move |_event: web_sys::MouseEvent| {
-            *IS_MOUSE_DOWN.lock().unwrap() = false;           
+            IS_MOUSE_DOWN.store(false, Relaxed);       
         }) as Box<dyn FnMut(web_sys::MouseEvent)>);
         window
             .add_event_listener_with_callback("mouseup", event.as_ref().unchecked_ref())
@@ -59,7 +60,7 @@ pub fn start_recording_mouse_events() {
         event.forget();
 
         let event = Closure::wrap(Box::new(move |_event: web_sys::MouseEvent| {
-            *IS_MOUSE_IN_CANVAS.lock().unwrap() = true;
+            IS_MOUSE_IN_CANVAS.store(true, Relaxed);
         }) as Box<dyn FnMut(web_sys::MouseEvent)>);
         window
             .add_event_listener_with_callback("mouseenter", event.as_ref().unchecked_ref())
@@ -67,7 +68,7 @@ pub fn start_recording_mouse_events() {
         event.forget();
 
         let event = Closure::wrap(Box::new(move |_event: web_sys::MouseEvent| {
-            *IS_MOUSE_IN_CANVAS.lock().unwrap() = false;
+            IS_MOUSE_IN_CANVAS.store(false, Relaxed);
         }) as Box<dyn FnMut(web_sys::MouseEvent)>);
         window
             .add_event_listener_with_callback("mouseleave", event.as_ref().unchecked_ref())
@@ -82,7 +83,7 @@ pub fn start_recording_mouse_events() {
             .unwrap();
         event.forget();
 
-        *IS_RECORDING_MOUSE_EVENTS.lock().unwrap() = true;
+        IS_RECORDING_MOUSE_EVENTS.store(true, Relaxed);
     } else {
         crate::elog!("Your program is calling start_recording_mouse_events() multiple times! That's bad!");
     }
@@ -91,7 +92,7 @@ pub fn start_recording_mouse_events() {
 /// Return true if the left button of the mouse is currently pressed.
 /// Make sure you called [start_recording_mouse_events()](fn.start_recording_mouse_events.html) before.
 pub fn is_mouse_pressed() -> bool {
-    *IS_MOUSE_DOWN.lock().unwrap()
+    IS_MOUSE_DOWN.load(Relaxed)
 }
 
 /// Return the current position of the mouse.
